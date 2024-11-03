a779294fc401851621a3ace4e5b54e9bb2f9e89e
Sami Chougle <samichougle.saac@gmail.com>
2024-11-03 16:09:32 +0530

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by sami chougle on 03/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
